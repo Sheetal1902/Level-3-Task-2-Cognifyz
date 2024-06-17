661c4a22da894aa3fd50b802134526149b763e33
Sheetal1902 <sheetalbirla889@gmail.com>
2024-06-16 22:56:03 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+
+document.getElementById('ctaButton').addEventListener('click', function() {
+    alert('Thank you for your interest! Please send your resume to internships@cognifyz.com.');
+});
